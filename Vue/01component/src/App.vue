d9cc5a228ba291f157d2c82da2f2730ae85bec38
borealRoc <xspstudyemail.com>
2020-03-17 12:21:25 +0800

    添加vue-router

--- Vue/01component/src/App.vue
+++ Vue/01component/src/App.vue
@@ -1,36 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ComPratice msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ComPratice from './components/ComPratice.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ComPratice
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  padding: 0;
+  margin: 0;
 }
 </style>
---
+++ Vue/01component/src/components/ComPratice.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="com-pratice">
+    <h1 class="com-pratice-hd">Vue组件化实践</h1>
+    <div class="com-pratice-ctn">
+      <ul class="com-lists">
+        <li class="com-list">
+          <h2>1. 组件传值、通信</h2>
+          <dl>
+            <dt>1.1 父组件 => 子组件</dt>
+            <dd>1.1.1 属性props</dd>
+            <dd>1.1.2 引用refs</dd>
+            <dd>1.1.3 子组件chidren</dd>
+          </dl>
+          <dl>
+            <dt>1.2 子组件 => 父组件</dt>
+            <dd>1.2.1 自定义事件</dd>
+          </dl>
+          <dl>
+            <dt>1.3 兄弟组件</dt>
+            <dd>1.3.1 通过共同的祖辈组件搭桥，$parent或$root</dd>
+          </dl>
+          <dl>
+            <dt>1.4 祖先和后代之间</dt>
+            <dd>1.4.1 provide/inject：能够实现祖先给后代传值</dd>
+          </dl>
+          <dl>
+            <dt>1.5 任意两个组件之间</dt>
+            <dd>1.5.1 事件总线：创建一个Bus类负责事件派发、监听和回调管理</dd>
+            <dd>1.5.2 Vuex</dd>
+          </dl>
+        </li>
+        <li class="com-list">
+          <h2>2. 插槽</h2>
+          <dl>
+            <dd>2.1 匿名插槽</dd>
+            <dd>2.2 具名插槽</dd>
+            <dd>2.3 作用域插槽</dd>
+          </dl>
+        </li>
+      </ul>
+      <div class="com-ctt">
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ComPratice",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.com-pratice-hd {
+  text-align: center;
+  line-height: 100px;
+}
+.com-pratice-ctn {
+  display: flex;
+}
+.com-lists {
+  flex: 1;
+}
+.com-ctt {
+  flex: 2;
+}
+h1 {
+  color: red;
+}
+h2 {
+  color: green;
+}
+dt {
+  color: blue;
+  padding-left: 1em;
+}
+dd {
+  padding-left: 2em;
+}
+dl {
+  line-height: 2;
+}
+</style>
