92108fded13f8e9390e971edf2df671b50ccfc0f
borealRoc <xspstudyemail.com>
2020-03-24 14:34:41 +0800

    动态组件实例的创建--弹窗组件

--- Vue/01component/src/utils/create.js
+++ Vue/01component/src/utils/create.js
@@ -8,15 +8,15 @@ function create(Component, props) {
         // 问：为啥用render而不用template:'', ？
         // 答：由于没有编译器，这里只能使用render
         // 1.2 h是createElement别名，返回虚拟DOM[VNode]
-        // 参数2props是要传递给Component的参数，{props: props, on: {click:onClick}}
+        // 1.3 参数props是要传递给Component的参数，{props: props, on: {click:onClick}}
         // 比如：<Component title content/>
         render: h => h(Component, { props })
-        // 1.3 $mount()会把上面生存的VNode转化成真是DOM，并挂载到目标节点上
+        // 1.4 $mount()会把上面生成的VNode转化成真是DOM，并挂载到目标节点上
         // 若不指定选择器，会执行转化过程，只是不挂载
     }).$mount()
 
     // 2.手动挂载：必须使用原生dom api把它插入文档中
-    // 2.1 vm.$el真实dom元素
+    // 2.1 vm.$el：真实dom元素
     document.body.appendChild(vm.$el)
 
     // 3.回收：防止内存泄漏
