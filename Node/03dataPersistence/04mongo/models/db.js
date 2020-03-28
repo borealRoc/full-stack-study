8034fa219759b6d912ca2c90a9feed3c0f5574a9
xusp <xusp@weizhenzhendeiMac-2.local>
2020-03-28 16:48:23 +0800

    mongodb

---
+++ Node/03dataPersistence/04mongo/models/db.js
@@ -0,0 +1,30 @@
+const conf = require('./conf')
+const { EventEmitter } = require('events')
+const { MongoClient } = require('mongodb')
+
+class Mongodb {
+    constructor(conf) {
+        this.conf = conf
+        this.emmiter = new EventEmitter()
+        // 1. 创建客户端
+        this.client = new MongoClient(conf.url, {
+            useNewUrlParser: true,
+        })
+        // 2. 创建连接
+        this.client.connect(err => {
+            if (err) throw err
+            console.log('连接成功')
+            this.emmiter.emit('connect')
+        })
+    }
+    // 3. 创建数据库dbName和集合colName
+    col(dbName = conf.dbName, colName){
+        return this.client.db(dbName).collection(colName)
+    }
+
+    once(event,cb){
+        this.emmiter.once(event,cb)
+    }
+}
+
+module.exports = new Mongodb(conf)
