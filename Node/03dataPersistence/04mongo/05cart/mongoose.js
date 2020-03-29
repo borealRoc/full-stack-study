003eeb154284f6002195ed3db85b424859e89623
xusp <xusp@weizhenzhendeiMac-2.local>
2020-03-29 15:16:25 +0800

    整理目录

--- Node/03dataPersistence/04mongo/05cart/mongoose.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-// 1.连接
-mongoose.connect("mongodb://localhost:27017/mongoose", { useNewUrlParser: true });
-const conn = mongoose.connection;
-conn.on("error", () => console.error("连接数据库失败"));
