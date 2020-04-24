3890c74d205d5a1f01f5fdb7f3f3dcff75266e65
xusp <xusp@weizhenzhendeiMac-2.local>
2020-04-24 18:16:52 +0800

    泛型和装饰器

--- Vue/05vue-ts/src/components/TSTest.vue
+++ Vue/05vue-ts/src/components/TSTest.vue
@@ -39,7 +39,6 @@ const log = (mes: string): void => {
 };
 log("函数返回值void类型表示无返回值");
 
-
 // 2. 函数
 // 2.1 ts函数中参数如果声明了，就是必选参数[除非参数后面加‘？’]
 // 可选参数放在在必选参数后面
@@ -60,7 +59,6 @@ function info(a: { name: string } | string): any {
 console.log(info({ name: "-xu-" }));
 console.log(info("xuxuxu"));
 
-
 // 3. 类
 // 3.1 修饰符：TypeScript里，成员都默认为 public
 // 3.1.1 public：可以自由的访问类里定义的成员
@@ -75,56 +73,56 @@ console.log(info("xuxuxu"));
 // 暴露私有属性
 // 在vue里面可以用作计算属性
 class Person {
-  public public_name: string
-  private private_name: string
-  protected protected_name: string
-  readonly readonly_name: string = 'i am readonly'
-  static static_age: number = 17
+  public public_name: string;
+  private private_name: string;
+  protected protected_name: string;
+  readonly readonly_name: string = "i am readonly";
+  static static_age: number = 17;
 
   constructor(theName1: string, theName2: string, theName3: string) {
-    this.public_name = theName1
-    this.private_name = theName2
-    this.protected_name = theName3
+    this.public_name = theName1;
+    this.private_name = theName2;
+    this.protected_name = theName3;
   }
   sayAge() {
-    console.log('static_age', Person.static_age)
+    console.log("static_age", Person.static_age);
   }
   // 暴露私有属性name2
-  get privateName () {
-    return this.private_name
+  get privateName() {
+    return this.private_name;
   }
-  set privateName (val) {
-    this.private_name = val
-  } 
+  set privateName(val) {
+    this.private_name = val;
+  }
 }
 
 class Employee extends Person {
   constructor(public_name, private_name, protected_name) {
-    super(public_name, private_name, protected_name)
+    super(public_name, private_name, protected_name);
   }
   sayName2() {
     // Property 'name2' is private and only accessible within class 'Person'
     // console.log(this.name2)
   }
   sayName3() {
-    console.log(this.protected_name)
+    console.log(this.protected_name);
   }
 }
-const Person1 = new Person('public_name', 'private_name', 'protected_name')
-console.log(Person1.public_name) //public_name
-// Property 'private_name' is private and only accessible within class 'Person' 
-// console.log(Person1.private_name) 
+const Person1 = new Person("public_name", "private_name", "protected_name");
+console.log(Person1.public_name); //public_name
+// Property 'private_name' is private and only accessible within class 'Person'
+// console.log(Person1.private_name)
 // Property 'protected_name' is protected and only accessible within class 'Person' and its subclasses.
 // console.log(Person1.protected_name)
-const Person2 = new Employee('Employee1', 'Employee2', 'Employee3')
-Person2.sayName3() //Employee3
-console.log('readonly', Person1.readonly_name) // i am readonly
+const Person2 = new Employee("Employee1", "Employee2", "Employee3");
+Person2.sayName3(); //Employee3
+console.log("readonly", Person1.readonly_name); // i am readonly
 // Cannot assign to 'readonly_name' because it is a read-only property.
 // Person1.readonly_name = 'modify readonly'
-Person1.sayAge() //static_age 17
-console.log('通过get访问到了Person的私有属性', Person1.privateName) //private_name
-Person1.privateName = 'set Person1.privateName2'
-console.log('通过set设置了Person的私有属性', Person1.privateName) //set Person1.privateName2
+Person1.sayAge(); //static_age 17
+console.log("通过get访问到了Person的私有属性", Person1.privateName); //private_name
+Person1.privateName = "set Person1.privateName2";
+console.log("通过set设置了Person的私有属性", Person1.privateName); //set Person1.privateName2
 
 // 3.3 类可以用来约束数据结构
 class Shape {
@@ -148,8 +146,8 @@ class Square extends Shape {
     return "我是" + this.color + " 面积是" + this.area + "平方厘米";
   }
 }
-const square: Square = new Square("blue", 2)
-console.log(square.shoutout()) //我是blue 面积是4平方厘米
+const square: Square = new Square("blue", 2);
+console.log(square.shoutout()); //我是blue 面积是4平方厘米
 
 // 4. 接口
 // 4.1 interface，仅用来约束数据结构，不做具体方法实现
@@ -159,16 +157,38 @@ interface MySelf {
   lastName: string;
   sayHello(): string; //要求以Person接口为数据结构约束的变量必须有sayHello方法
 }
-const greeting = (person: MySelf) => person.sayHello()
+// 面向接口编程
+const greeting = (person: MySelf) => person.sayHello();
 const user = {
   firstName: "Xu",
   lastName: "King",
   sayHello: function() {
     return `Hello, ${this.firstName} ${this.lastName}`;
   }
+};
+console.log("面向接口编程", greeting(user)); //Hello, Xu King
+// 4.3 用接口去强制一个类去符合某种契约
+// 接口描述了类的公共部分，而不是公共和私有两部分。 它不会检查类是否具有某些私有成员
+class Me implements MySelf {
+  constructor(public firstName = "", public lastName = "") {}
+  sayHello() {
+    return "Hello, " + this.firstName + " " + this.lastName;
+  }
 }
-console.log('接口', greeting(user)) //Hello, Xu King
+const me = new Me("__xu__", "--shao--");
+console.log("类实现接口", me.sayHello()); //Hello, __xu__ --shao--
 
+// 5. 泛型
+// 泛型是指在定义函数，接口或类的时候，不预先指定具体的类型， 而在使用的时候再指定类型的一种特性
+// 定义泛型接口
+interface Result<T> {
+  ok: 0 | 1;
+  data: T[];
+}
+// 定义泛型函数
+
+// 6. 装饰器
+// 装饰器实际上是工厂函数，传人一个对象，输出处理后的新对象
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
