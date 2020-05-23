34034b032872ad331b7723264c8fa0ca9971a37c
xusp <xusp@weizhenzhendeiMac-2.local>
2020-05-23 16:27:49 +0800

    修改项目的webpack配置

--- React/01react-core/config-overrides.js
+++ React/01react-core/config-overrides.js
@@ -1,5 +1,11 @@
-const { override, addDecoratorsLegacy } = require("customize-cra")
+const { override, fixBabelImports, addDecoratorsLegacy } = require("customize-cra");
 
 module.exports = override(
+    fixBabelImports("import", {
+        libraryName: "antd",
+        // antd库的目录：/Users/xusp/Desktop/studySpace/full-stack-study/React/01react-core/node_modules/antd/es
+        libraryDirectory: "es",
+        style: "css",
+    }),
     addDecoratorsLegacy(), //配置装饰器
 );
