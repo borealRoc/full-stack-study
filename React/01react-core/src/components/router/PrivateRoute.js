723040df5da863ee017f0ed5921519e3dfa59b74
xusp <xusp@weizhenzhendeiMac-2.local>
2020-06-08 16:40:50 +0800

    路由守卫

--- React/01react-core/src/components/router/PrivateRoute.js
+++ React/01react-core/src/components/router/PrivateRoute.js
@@ -7,8 +7,6 @@ const mapStateToProps = state => { return { userInfo: state.loginReducer } }
 class PrivateRoute extends Component {
     render() {
         const { component: Comp, userInfo, ...rest } = this.props
-        console.log('PrivateRoute--props', this.props)
-        console.log('PrivateRoute--rest', rest)
         const { isLogin } = userInfo
         return (
             <Route {...rest} render={props =>
