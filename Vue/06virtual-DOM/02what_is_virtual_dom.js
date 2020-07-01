65861d0ba3b69d4867f5c0b9de48d9db1e0ebe8d
xusp <xusp@weizhenzhendeiMac-2.local>
2020-07-01 17:19:25 +0800

    什么是虚拟dom

---
+++ Vue/06virtual-DOM/02what_is_virtual_dom.js
@@ -0,0 +1,23 @@
+{/* <div id="app">
+    <p class='item'>子节点</p>
+</div> */}
+
+const createElement = () => {
+    return {
+        tag: 'div',
+        data: {
+            id: 'app'
+        },
+        children: [
+            {
+                tag: 'p',
+                data: {
+                    class: 'item',
+                },
+                children: ['子节点']
+            }
+        ]
+    }
+}
+
+// 虚拟DOM就是用JS对象描述真实DOM
