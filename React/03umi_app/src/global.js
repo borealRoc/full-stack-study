d953a318c5e045b3784e0ccb8d6524d2a9cb38ac
xusp <xusp@weizhenzhendeiMac-2.local>
2020-07-30 09:47:10 +0800

    删除一些东西

--- React/03umi_app/src/global.js
+++
@@ -1,3 +0,0 @@
-// 全局入口
-/* eslint-disable */
-import interceptor from './interceptor'
--- React/03umi_app/src/pages/404.js
+++
@@ -1,5 +0,0 @@
-import { Exception } from "ant-design-pro";
-function ErrorPage() {
-    return <Exception type="404" backText="返回首页"></Exception>;
-}
-export default ErrorPage
