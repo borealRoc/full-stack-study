c4e9f43c34f56033f2e4cc5bec15a353021cf78d
borealRoc <xspstudyemail.com>
2020-09-10 12:17:51 +0800

    微信开发

---
+++ Mobile/01weixin/index.js
@@ -0,0 +1,109 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+const static = require('koa-static')
+const bodyParser = require('koa-bodyparser');
+const app = new Koa()
+app.use(bodyParser())
+const router = new Router()
+app.use(static(__dirname + '/'))
+
+const axios = require('axios')
+const conf = require('./conf')
+
+// 1 微信服务端 -- 获取用户 token 和获取关注列表
+// 1.1 原生调用
+// const tokenCache = {
+//     access_token: '',
+//     updateTime: Date.now(),
+//     expires_in: 7200,
+// }
+// router.get('/getTokens', async ctx => {
+//     const wxDomain = `https://api.weixin.qq.com`;
+//     const path = `/cgi-bin/token`;
+//     const params = `?grant_type=client_credential&appid=${conf.appid}&secret=${conf.appsecret}`;
+//     const url = `${wxDomain}${path}${params}`;
+//     const res = await axios.get(url);
+//     Object.assign(tokenCache, res.data, {
+//         updateTime: Date.now()
+//     });
+//     ctx.body = res.data
+// })
+// router.get('/getFollowers', async ctx => {
+//     const url = `https://api.weixin.qq.com/cgi-bin/user/get?access_token=${tokenCache.access_token}`
+//     const res = await axios.get(url)
+//     ctx.body = res.data
+// })
+
+// 1.2 借助co-wechat-api调用
+const WechatAPI = require('co-wechat-api')
+const { ServerToken,ClientToken } = require('./mongoose')
+const api = new WechatAPI(conf.appid, conf.appsecret,
+    async function () {
+        return await ServerToken.findOne()
+    },
+    async function (token) {
+        const res = await ServerToken.updateOne({}, token, { upsert: true })
+    }
+);
+router.get('/getFollowers', async ctx => {
+    let res = await api.getFollowers();
+    res = await api.batchGetUsers(res.data.openid, 'zh_CN');
+    ctx.body = res
+})
+
+
+// 2. 微信网页端
+// 2.1 微信第三方登陆
+const OAuth = require('co-wechat-oauth')
+const oauth = new OAuth(conf.appid, conf.appsecret,
+    async function (openid) {
+        return await ClientToken.getToken(openid)
+    },
+    async function (openid, token) {
+        return await ClientToken.setToken(openid, token)
+    }
+)
+/**
+ * 2.1.1 生成用户URL
+ */
+router.get('/wxAuthorize', async (ctx, next) => {
+    const state = ctx.query.id
+    console.log('ctx.query.id: ', state)
+    console.log('ctx.href: ', ctx.href)
+    let redirectUrl = ctx.href
+    redirectUrl = redirectUrl.replace('wxAuthorize', 'wxCallback')
+    console.log('redirectUrl: ', redirectUrl)
+    const scope = 'snsapi_userinfo'
+    // const scope = 'snsapi_base'
+    const url = oauth.getAuthorizeURL(redirectUrl, state, scope)
+    console.log('url: ', url)
+    ctx.redirect(url)
+})
+/**
+ *  2.1.2 用户回调方法
+ */
+router.get('/wxCallback', async ctx => {
+    const code = ctx.query.code //授权码
+    console.log('ctx.query.code: ', code)
+    const token = await oauth.getAccessToken(code)
+    const accessToken = token.data.access_token
+    const openid = token.data.openid
+    console.log('accessToken', accessToken)
+    console.log('openid', openid)
+    ctx.redirect('/?openid=' + openid)
+})
+/**
+ *  2.1.3 获取用户信息
+ */
+router.get('/getUser', async ctx => {
+    const openid = ctx.query.openid
+    console.log('/getUser openid: ', openid)
+    const userInfo = await oauth.getUser(openid)
+    console.log('userInfo:', userInfo)
+    ctx.body = userInfo
+})
+
+
+app.use(router.routes()); /*启动路由*/
+app.use(router.allowedMethods());
+app.listen(3000);
--- Mobile/note/01weixin.md
+++ Mobile/note/01weixin.md
@@ -2,7 +2,7 @@
 ## 开发环境
 1. 注册微信订阅号 <https://mp.weixin.qq.com/>
 2. sunny-ngrok (内网穿透tunnel) <https://www.ngrok.cc/>
-## 实战
+## 公众号服务端
 1. 客服消息
     - 方法1: 微信公众号平台设置 <https://mp.weixin.qq.com/cgi-bin/frame?t=advanced/dev_tools_frame&nav=10049&token=1003798924&lang=zh_CN>
     - 方法2: 开通公众号测试账号 <http://mp.weixin.qq.com/debug/cgi-bin/sandboxinfo?action=showinfo&t=sandbox/index>, 借助`npm i co-wechat -S`, 在自己的服务器写代码实现
@@ -12,3 +12,29 @@
     - 方法2: 微信公众号开发文档 <https://developers.weixin.qq.com/doc/offiaccount/Getting_Started/Overview.html>, 借助 `npm i 'co-wechat-api -S`
 4. 多进程下保存 token 全局票据
     - 将 token 保存到数据库
+## 公众号网页端
+### 一、网页授权获取用户基本信息
+1. 原理 -- OAuth2.0  
+<img src="oauth2_1.png"/>  
+<img src="oauth2_2.png"/>  
+
+2. 开发过程
+- 2.1 微信公众号平台授权
+    - 2.1.1 获取 “网页授权 -- 网页授权获取用户基本信息” 的权限 
+        - <https://mp.weixin.qq.com/advanced/advanced?action=table&token=1899850966&lang=zh_CN>
+        - 有一个疑问：服务号支持，订阅号不支持？
+    - 2.1.2 修改授权回调域名
+        - <https://mp.weixin.qq.com/debug/cgi-bin/sandboxinfo?action=showinfo&t=sandbox/index>
+- 2.2 借助 `npm i co-wechat-oauth -S` 开发
+    - 2.2.1 生成用户URL： `oauth.getAuthorizeURL`
+    - 2.2.2 获取用户回调 AccessToken 与 OpenId
+    ```javascript
+    let token = await oauth.getAccessToken(code)
+    let accessToken = token.data.access_token
+    let openid = token.data.openid
+    ```
+    - 2.2.3 用户信息: `let userInfo = oauth.getUser(openid)`
+    - 2.2.4 AccessToken缓存: 保存到数据库
+## 二、JSSDK
+
+
