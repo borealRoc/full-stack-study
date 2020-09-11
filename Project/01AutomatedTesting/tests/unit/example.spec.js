91fa19afe5897ce41bc745658a1d9d30ebcf5d5d
borealRoc <xspstudyemail.com>
2020-09-11 20:11:56 +0800

    测试husky

--- Project/01AutomatedTesting/tests/unit/example.spec.js
+++ Project/01AutomatedTesting/tests/unit/example.spec.js
@@ -18,7 +18,7 @@ function add(num1, num2) {
 }
 describe('unit_test_demo', () => {
   it('测试加法', () => {
-    expect(add(1, 3)).toBe(7)
+    expect(add(1, 3)).toBe(8)
     expect(add(1, 3)).toBe(4)
     expect(add(-2, 3)).toBe(1)
   })
