154e8ec7d104046067d9733dc66d4c4fdeae1e65
borealRoc <xspstudyemail.com>
2020-09-11 20:05:33 +0800

    加法测试

--- Project/01AutomatedTesting/tests/unit/example.spec.js
+++ Project/01AutomatedTesting/tests/unit/example.spec.js
@@ -10,3 +10,16 @@ describe('HelloWorld.vue', () => {
     expect(wrapper.text()).toMatch(msg)
   })
 })
+
+
+// 1. 测试加法
+function add(num1, num2) {
+  return num1 + num2
+}
+describe('unit_test_demo', () => {
+  it('测试加法', () => {
+    expect(add(1, 3)).toBe(3)
+    expect(add(1, 3)).toBe(4)
+    expect(add(-2, 3)).toBe(1)
+  })
+})
