aacf886fa1e615bd7449f7f8143c7a1aea465b44
borealRoc <xspstudyemail.com>
2020-09-15 16:31:59 +0800

    分页

--- Mobile/02MiniProgram/02cloud/miniprogram/pages/bookLists/bookLists.js
+++ Mobile/02MiniProgram/02cloud/miniprogram/pages/bookLists/bookLists.js
@@ -6,7 +6,45 @@ Page({
    * 页面的初始数据
    */
   data: {
-    books: []
+    books: [],
+    page: 0,
+  },
+
+  // 获取图书列表
+  getBookLists() {
+    let isInit = this.data.page === 0
+
+    wx.showLoading({ title: '加载中' })
+    // 每页显示5个
+    const PAGE_ACCOUNT = 5
+    // 第 n 页要显示的数目
+    const offset = this.data.page * PAGE_ACCOUNT
+    let ret = db.collection('books_collections').orderBy('create_time', 'desc')
+    if (offset > 0) {
+      // 不是第一页
+      ret = ret.skip(offset)
+    }
+    ret = ret.limit(PAGE_ACCOUNT).get().then(books => {
+      let newBooks = books.data.map(book => {
+        const rate = Math.round(book.rate / 2)
+        book.rateStart = "★★★★★☆☆☆☆☆".slice(5 - rate, 10 - rate)
+        return book
+      })
+      if (isInit) {
+        // 不是第一页
+        this.setData({
+          books: newBooks
+        })
+      } else {
+        // 第一页
+        this.setData({
+          books: this.data.books.concat(newBooks)
+        })
+      }
+    })
+    setTimeout(() => {
+      wx.hideLoading()
+    }, 1000)
   },
 
   /**
@@ -14,18 +52,7 @@ Page({
    */
   onLoad: function (options) {
     // 查库
-    db.collection('books_collections').get({
-      success: books => {
-        console.log('books', books)
-        this.setData({
-          books: books.data.map(book => {
-            const rate = Math.round(book.rate / 2)
-            book.rateStart = "★★★★★☆☆☆☆☆".slice(5 - rate, 10 - rate)
-            return book
-          })
-        })
-      }
-    })
+    this.getBookLists()
   },
 
   /**
@@ -60,14 +87,23 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    // 下拉刷新
+    this.setData({
+      page: 0
+    }, () => {
+      this.getBookLists()
+    })
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    this.setData({
+      page: this.data.page + 1
+    }, () => {
+      this.getBookLists()
+    })
   },
 
   /**
